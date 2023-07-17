80e25864dfbc6d07449e2479b1d2334b103f205f
elise <Elisenovaes10@gmail.com>
2023-07-17 18:48:33 -0300

    Alteração do componente Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Button = ({ value, onClick }) => {
   return (
     <div>
-      <button className='Button' value={value} onClick={onClick}>
+      <button value={value} onClick={onClick}>
         {value}
       </button>
     </div>
